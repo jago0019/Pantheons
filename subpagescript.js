7f4213b67d7292d403e56ae1a4a8e0336dca0c6e
jakobgottfridsson <jakob.gottfridsson@gmail.com>
2025-03-31 13:33:05 +0200

    new changes dawg

--- subpagescript.js
+++ subpagescript.js
@@ -4,6 +4,37 @@ const norseBanner = document.getElementById('norse-banner');
 const greekBanner = document.getElementById('greek-banner');
 const egyptianBanner = document.getElementById('egyptian-banner');
 
+document.querySelectorAll('.god').forEach(god => {
+    god.addEventListener('click', () => {
+        localStorage.removeItem("godname");
+        localStorage.removeItem("godimage");
+        localStorage.removeItem("godinfo");
+        localStorage.removeItem("godparagraph");
+
+        let godName = god.querySelector('.god-details h3').innerHTML;
+        let godImg = god.querySelector('.god-image img').src.split('/').pop();
+        let godPara = god.querySelector('.god-image img').dataset.paragraph;
+        let godInfo = god.querySelector('.god-details h4').innerHTML;
+
+
+        if (godName) {
+            localStorage.setItem("godname", godName);
+            localStorage.setItem("godimage", godImg);
+            localStorage.setItem("godinfo", godInfo);
+            localStorage.setItem("godparagraph", godPara);
+            console.log('saved name: ', localStorage.getItem("godname"));
+            console.log('saved image: ', localStorage.getItem("godimage"));
+            console.log('saved tagline: ', localStorage.getItem("godinfo"));
+            console.log('saved info: ', localStorage.getItem("godparagraph"));
+
+            window.location.href = '/goddetails.html';
+        } else {
+            console.log('No name found inside this god element.');
+        }
+
+    });
+});
+
 hamburgerButton.addEventListener('click', showMenu);
 
 function showMenu() {
